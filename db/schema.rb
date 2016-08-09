a81fe8a4aa09478749245671cf4d5dc71d7e3fb0
Michael Delbes <michael.delbes@ece-france.com>
2016-08-09 12:13:05 +0200

    Adding Controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809093836) do
+ActiveRecord::Schema.define(version: 20160809094942) do
 
   create_table "task_managers", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160809093836) do
     t.boolean  "done"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.date     "deadline"
   end
 
 end
