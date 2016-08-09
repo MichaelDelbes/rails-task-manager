b7c6eab7b1aabcb1458aef09875e2c3939bbda16
Michael Delbes <michael.delbes@ece-france.com>
2016-08-09 12:53:07 +0200

    Task controller MAJ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.all
   end
 
   def show
+    @task =
   end
 
   def new
@@ -12,11 +14,14 @@ class TasksController < ApplicationController
   end
 
   def edit
+     @task = Task.find(params[:id])
   end
 
   def update
+     @task = Task.find(params[:id])
   end
 
   def destroy
+     @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
 
- get '/tasks' => 'tasks#new'
+get '/index' => 'tasks#index'
+
+get '/show' => 'tasks#show'
+
+get '/new' => 'tasks#new'
+
+get '/create' => 'tasks#create'
+
+get '/edit' => 'tasks#edit'
+
+get '/update' => 'tasks#update'
+
+get '/destroy' => 'tasks#destroy'
+
 
 end
